4ddff832e825fe4fd2bc973f698645a657fbec3c
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2025-03-13 19:21:36 +0100

    add index file

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,22 @@
 import { IoMdPerson } from 'react-icons/io';
 import { HiPhone } from 'react-icons/hi2';
+import { MdDelete } from 'react-icons/md';
 import css from './Contact.module.css';
 
-const Contact = ({ name, number, icon }) => {
+const Contact = ({ id, name, number, onDelete }) => {
   return (
     <div className={css.contact}>
       <div className={css.nameBox}>
-        <IoMdPerson>{icon}</IoMdPerson>
+        <IoMdPerson className={css.icon} />
         <span className={css.name}>{name}</span>
       </div>
       <div className={css.phoneBox}>
-        <HiPhone>{icon}</HiPhone>
+        <HiPhone className={css.phoneIcon} />
         <span className={css.phone}>{number}</span>
       </div>
+      <button className={css.deleteBtn} onClick={() => onDelete(id)}>
+        <MdDelete className={css.deleteIcon} />
+      </button>
     </div>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,18 +3,13 @@ import css from './ContactList.module.css';
 
 const ContactList = ({ persons, onDelete }) => {
   return (
-    <>
-      <ul className={css.list}>
-        {persons.map(({ id, name, number }) => (
-          <li key={id} className={css.item}>
-            <Contact name={name} number={number} />
-            <button className={css.btn} onClick={() => onDelete(id)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className={css.list}>
+      {persons.map(({ id, name, number }) => (
+        <li key={id} className={css.item}>
+          <Contact id={id} name={name} number={number} onDelete={onDelete} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
